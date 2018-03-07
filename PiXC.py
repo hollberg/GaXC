dffbfe334fffb11e4b8978ad7465fe9e8f21b84e
Mitchell.Hollberg <mhollberg@gmail.com>
2018-03-07 16:52:30 -0500

    Updated comments

--- PiXC.py
+++ PiXC.py
@@ -3,29 +3,29 @@ Given the "base" url of a cross-country race:
     1) Pull hyperlinks to the results of all EVENTS at that race
     2) Navigate to each results page, save copy of results to a *.csv file"""
 
-import requests
-from bs4 import BeautifulSoup
 import config
 import csv
 
 links_list = []     # Stores links to all EVENTS
 results_list = []   # Stores all individual results
 
+# One RACE has many EVENTS: Loop over all race URLs; return urls for each EVENT
 for race, race_link in config.races.items():
     race_page = config.get_soup(race_link)
     results_table = race_page.find('table', {'class': 'meetResultsList'})
 
     if results_table != None:
-        age_group_links = results_table.find_all('a')
+        age_group_links = results_table.find_all('a')   # Get all links
         if age_group_links != None:
             for link in age_group_links:
                 url = link.attrs['href']
+                # Ensure the URL points to the "Raw" formatted data
                 url = url.replace('auto', 'raw')
                 if url[-1] == r'/':
                     url += 'raw'  # Ensure all links end with 'raw'
                 links_list.append((race,link.text.strip(), 'http://ga.milesplit.com'+ url, ))
 
-# print('***LINKS LIST***')
+# Write list of Links to each event results page to file
 with open('event_links.csv', 'w', newline='') as csvfile:
     event_links_writer = csv.writer(csvfile, delimiter='|' )
     event_links_writer.writerow(['Meet', 'Event', 'Link'])
@@ -33,22 +33,26 @@ with open('event_links.csv', 'w', newline='') as csvfile:
         # print(row)
         event_links_writer.writerow(row)
 
-# print('*** MEET RESULTS***')
+# With page for each EVENT results identifed, loop over all results lists
 for event in links_list:
-    event_results = get_soup(event[2])
+    event_results = config.get_soup(event[2])  # the URL to results page
     event_data = event_results.find('div', {'id': 'meetResultsBody'})
-    header_reached = False
-    parse_type = ''
+    header_reached = False  # Flag becomes True when we get to header row
+    parse_type = ''         # Will specify fixed-len parsing to use
 
     if event_data == None:
         print(f'***No <pre> text at :{event}***')
         continue
 
     for i, row in enumerate(event_data.pre.text.split('\n')):
+        # See if header row like 'Name Year School...'
+        # If so, use the fixed-width spacing per "Perfect Timing Group" format
         if row.replace(' ', '').startswith('NameYearSchoolFinalsPoint'):
             parse_type = config.perfect_timing_parse
             header_reached = True
             header_row = i
+        # See if header row like "Pl Athlete Yr..."
+        # If so, will use fixed-width spacer per Fast Feet Timing
         if row.replace(' ','').startswith('PlAthleteYr'):
             parse_type = config.fast_feet_parse
             header_reached = True
